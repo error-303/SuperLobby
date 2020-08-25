23c443c4a966e061f62a266903a806cf1c2eaa96
error-303 <69956752+error-303@users.noreply.github.com>
2020-08-25 11:17:37 +0200

    Update Main.php

--- src/Lobby/Main.php
+++ src/Lobby/Main.php
@@ -47,6 +47,7 @@ public function onEnable(){
 	$this->getLogger()->info("Lobby Plugin has been actived");
 	
 	$this->getServer()->getPluginManager()->registerEvents($this , $this);
+	$this->getScheduler()->scheduleRepeatingTask(new CountTask($this), 20)
 }
 
 
